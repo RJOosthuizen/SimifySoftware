0482be02cc5b19457d9121771e4b4c286f69f3e1
RJOosthuizen <33512291+RJOosthuizen@users.noreply.github.com>
2021-09-27 21:28:33 +0200

    Create base.ino

---
+++ Arduino/base/base.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+  // TODO: Add model name here so settings can be fetched
+  digitalWrite(3, LOW);
+}
